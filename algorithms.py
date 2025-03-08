1dc3d3dc794783450dc9900cb112958644c90fca
Damian Jasek <damiannq1337@gmail.com>
2025-03-08 15:21:59 +0100

    Add derivates, minor changes to main file

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,8 @@
-def bisection(input_range : list, f , epsilon: float) -> float:
+from collections.abc import Callable
+import numpy as np
+
+
+def bisection(input_range : list, f : Callable[[float | np.ndarray], float], epsilon: float) -> float:
     a = input_range[0]
     b = input_range[1]
     x = (a + b) / 2
@@ -14,8 +18,30 @@ def bisection(input_range : list, f , epsilon: float) -> float:
         x = (a + b) / 2
     return x
 
-def newton(input_range : list, f, d, iterations: int) -> float:
+# def bisection(input_range : list, f , iterations: int) -> float:
+#     a = input_range[0]
+#     b = input_range[1]
+#     x = (a + b) / 2
+# 
+#     if f(x) == 0:
+#         return x
+# 
+#     for i in range(iterations):
+#         if f(a) * f(x) < 0:
+#             b = x
+#         if f(x) * f(b) < 0:
+#             a = x
+#         x = (a + b) / 2
+#     return x
+
+def newton(input_range : list, f : Callable[[float | np.ndarray], float], d : Callable[[float | np.ndarray], float], epsilon: float) -> float:
     x = input_range[0]
-    for i in range(iterations):
+    while abs(f(x)) < epsilon:
         x = x - (f(x)/d(x))
     return x
+
+# def newton(input_range : list, f, d, iterations: int) -> float:
+#     x = input_range[0]
+#     for i in range(iterations):
+#         x = x - (f(x)/d(x))
+#     return x
--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 from re import sub
+from typing import Callable
 
-import functions as fn
-import derivates as dn
-import algorithms as al
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import ndarray
 
+from algorithms import bisection, newton
+from derivates import polymonial_derivate
 from functions import polymonial, trigonometric, exponential, composite
 
-if __name__ == '__main__':
-    choice : int = -1
-    input_range : list = [1, 0]
-    parameter : str = 'a'
+def prompt() -> int:
+    c = -1
+    while c < 0:
+        choice_input = input()
+        if choice_input.isnumeric():
+            c = int(choice_input)
+    return c
 
+if __name__ == '__main__':
+    input_range : list = [1, 0]
+    funcs = [polymonial, trigonometric, exponential, composite]
+    funcs_derivates = [polymonial_derivate, trigonometric, exponential, composite]
+    
     print("0. f(x) = 3x^2 + x + 5")
     print("1. g(x) = cos(x - 2)")
     print("2. h(x) = 0,5^x - x")
     print("3. k(x) = h(f(x))")
     print("4 - wyjście")
 
-    while choice < 0:
-        choice_input = input()
-        if choice_input.isnumeric():
-            choice = int(choice_input)
+    choice = prompt()
 
     print("[a, b]")
 
@@ -37,24 +43,28 @@ if __name__ == '__main__':
         input_range[1] = int(b_input)
 
     print("Kryterium")
-    print("a - Dokładność")
-    print("b - Liczba iteracji")
+    print("0 - Dokładność")
+    print("1 - Liczba iteracji")
 
-    # while parameter != 'a' or parameter != 'b':
-    #     parameter = input()
-
-    domain = np.linspace(input_range[0], input_range[1])
+    func : Callable[[float | ndarray], float] = funcs[choice]
+    func_derivate : Callable[[float | ndarray], float] = funcs_derivates[choice]
     
-    match choice:
-        case 0:
-            plt.plot(domain, polymonial(domain))
-        case 1:
-            plt.plot(domain, trigonometric(domain))
-        case 2:
-            plt.plot(domain, exponential(domain))
-        case 3:
-            plt.plot(domain, composite(domain))
+    domain = np.linspace(input_range[0], input_range[1])
+    plt.plot(domain, func(domain))
+    
+    choice2 = prompt()
 
+    match choice2:
+        case 0:
+            eps = float(input("Epsilon: "))
+            xb_0 = bisection(input_range, func, eps)
+            xn_0 = newton(input_range, func, func_derivate, eps)
+            plt.scatter(xb_0, func(xb_0))
+            plt.scatter(xn_0, func(xn_0))
+        # case 1:
+            # iterations = input("Iteracje: ")
+            # bisection(input_range, polymonial, iterations)
+            # newton(input_range, polymonial, , iterations)
     plt.show()
     
 
