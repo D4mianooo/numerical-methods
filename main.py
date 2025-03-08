4314b3c3524c4e913632073f48a4cfaab6f5b546
Damian Jasek <damiannq1337@gmail.com>
2025-03-08 14:29:16 +0100

    Modify main file

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
+from re import sub
+
 import functions as fn
 import derivates as dn
 import algorithms as al
 import numpy as np
 import matplotlib.pyplot as plt
 
-from functions import polymonial
+from functions import polymonial, trigonometric, exponential, composite
 
 if __name__ == '__main__':
-    # input_range = [-2, 2]
-    # print(al.newton(input_range, fn.polymonial, dn.polymonial_derivate, 5))
-    # zero = bisection(input_range, polymonial, 1e-10)
-    # t = np.linspace(input_range[0], input_range[1])
-    #
-    # plt.plot(t, polymonial(t))
-    # plt.grid(color='black', linestyle='-', linewidth=1)
-    # plt.scatter(zero, polymonial(zero))
-    # plt.show()
-
     choice : int = -1
     input_range : list = [1, 0]
-    # parameter : str = ""
+    parameter : str = 'a'
 
     print("0. f(x) = 3x^2 + x + 5")
-    print("1. f(x) = cos(x)")
-    print("3 - wyjście")
+    print("1. g(x) = cos(x - 2)")
+    print("2. h(x) = 0,5^x - x")
+    print("3. k(x) = h(f(x))")
+    print("4 - wyjście")
 
     while choice < 0:
         choice_input = input()
@@ -35,25 +29,32 @@ if __name__ == '__main__':
     while input_range[0] >= input_range[1]:
         a_input = input("a: ")
         b_input = input("b: ")
-        while not a_input.isnumeric():
+        while not sub('-', '', a_input).isnumeric():
             a_input = input("a: ")
-        while not b_input.isnumeric():
+        while not sub('-', '', b_input).isnumeric():
             b_input = input("b: ")
         input_range[0] = int(a_input)
         input_range[1] = int(b_input)
 
+    print("Kryterium")
+    print("a - Dokładność")
+    print("b - Liczba iteracji")
+
+    # while parameter != 'a' or parameter != 'b':
+    #     parameter = input()
+
+    domain = np.linspace(input_range[0], input_range[1])
+    
     match choice:
         case 0:
-            xes = np.linspace(input_range[0], input_range[1])
-            plt.plot(xes, polymonial(xes))
+            plt.plot(domain, polymonial(domain))
+        case 1:
+            plt.plot(domain, trigonometric(domain))
+        case 2:
+            plt.plot(domain, exponential(domain))
+        case 3:
+            plt.plot(domain, composite(domain))
 
     plt.show()
-    #
-    # print("Kryterium")
-    # print("a - Dokładność")
-    # print("b - Liczba iteracji")
-    #
-    # parameter_input : str = ""
-    # while parameter_input != 'a' or parameter_input != 'b':
-    #     parameter = parameter_input
+    
 
