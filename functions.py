0a2afb54697154ae198661fb730508e2c4957bd8
Damian Jasek <damiannq1337@gmail.com>
2025-03-08 14:28:19 +0100

    Add file

--- functions.py
+++ functions.py
@@ -1,13 +1,14 @@
 import math
+import numpy as np
 
-def polymonial(x : float) -> float:
+def polymonial(x : float | np.ndarray) -> float:
     return x*(x*x - 1) + 1
 
-def trigonometric(x : float) -> float:
+def trigonometric(x : float | np.ndarray) -> float:
     return math.cos(x - 2)
 
-def exponential(x : float) -> float:
+def exponential(x : float | np.ndarray) -> float:
     return pow(0.5, x) - x
 
-def composite(x : float) -> float:
+def composite(x : float | np.ndarray) -> float:
     return exponential(polymonial(x))
